6d3fcb924c20a4c1970c12f0aa86ce1fcfd5a3d5
Aluwani Nethavhakone <anethavhakone@jhb.dvt.co.za>
2017-05-11 19:11:37 +0200

    new view using angular

---
+++ VhakoneWeb/Controllers/ApplicantsController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VhakoneWeb.Models;
+
+namespace VhakoneWeb.Controllers
+{
+    [Authorize]
+    public class ApplicantsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            PersonalDetail personalDetails = db.PersonalDetails.FirstOrDefault(x => x.UserId == userId);
+
+            if (personalDetails == null)
+            {
+                ViewBag.canCreate = true;
+            }
+            else
+            {
+                ViewBag.canCreate = false;
+            }
+            return View();
+        }
+        // GET: Applicants
+        public JsonResult GetApplicant()
+        { 
+            var  userId = User.Identity.GetUserId();
+            PersonalDetail personalDetails = db.PersonalDetails.FirstOrDefault(x => x.UserId == userId);
+            return Json(personalDetails, JsonRequestBehavior.AllowGet);
+        
+        }
+
+        public string CreatePersonalDetail(PersonalDetail personalDetail)
+        {
+            if (personalDetail != null)
+            {
+                var userId = User.Identity.GetUserId();
+                var obj = new PersonalDetail()
+                {
+                    FirstName = personalDetail.FirstName,
+                    LastName = personalDetail.LastName,
+                    Title = personalDetail.Title,
+                    DateOfBirth = personalDetail.DateOfBirth,
+                    IdNumber = personalDetail.IdNumber,
+                    Gender = personalDetail.Gender,
+                    Disable = personalDetail.Disable,
+                    UserId = userId
+                };
+                db.PersonalDetails.Add(obj);
+                db.SaveChanges();
+                    return "Personal details Added Successfully";
+                
+            }
+            else
+            {
+                return "Personal details Not Inserted! Try Again";
+            }
+        }
+
+        public string EditPersonalDetail(PersonalDetail personalDetail)
+        {
+            if (personalDetail != null)
+            {
+                var userId = User.Identity.GetUserId();
+                var personalDetail_ = db.Entry(personalDetail);
+                PersonalDetail personalDetails = db.PersonalDetails.FirstOrDefault(x => x.UserId == userId);
+
+                personalDetails.FirstName = personalDetail.FirstName;
+                personalDetails.LastName = personalDetail.LastName;
+                personalDetails.Title = personalDetail.Title;
+                personalDetails.DateOfBirth = personalDetail.DateOfBirth;
+                personalDetails.IdNumber = personalDetail.IdNumber;
+                personalDetails.Gender = personalDetail.Gender;
+                personalDetails.Disable = personalDetail.Disable;
+                personalDetails.UserId = userId;
+                
+                db.SaveChanges();
+                return "PersonalDetail Updated Successfully";
+            }
+            else
+            {
+                return "PersonalDetail Not Updated! Try Again";
+            }
+        }
+    }
+}
---
+++ VhakoneWeb/Scripts/AngularCode.js
@@ -0,0 +1,80 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope, $http) {
+    debugger;
+    $scope.CreatePersonalDetail = function () {
+
+        $scope.personalDetail = {};
+        $scope.personalDetail.Title = $scope.Title;
+        $scope.personalDetail.FirstName = $scope.FirstName;
+        $scope.personalDetail.LastName = $scope.LastName;
+        $scope.personalDetail.Gender = $scope.Gender;
+        $scope.personalDetail.DateOfBirth = $scope.DateOfBirth;
+        $scope.personalDetail.IdNumber = $scope.IdNumber;
+        $scope.personalDetail.Disable = $scope.Disable;
+        $http({
+            method: "post",
+            url: "http://localhost:18327/Applicants/CreatePersonalDetail",
+            datatype: "json",
+            data: JSON.stringify($scope.personalDetail)
+        }).then(function (response) {
+            alert(response.data);
+            $scope.GetAllData();
+            $scope.Title = "";
+            $scope.FirstName = "";
+            $scope.LastName = "";
+            $scope.Gender = "";
+            $scope.DateOfBirth = "";
+            $scope.IdNumber = "";
+            $scope.Disable = "";
+        })
+    }
+    $scope.EditPersonalDetail = function () {
+        $scope.personalDetail = {};
+        $scope.personalDetail = {};
+        $scope.personalDetail.Title = $scope.Title;
+        $scope.personalDetail.FirstName = $scope.FirstName;
+        $scope.personalDetail.LastName = $scope.LastName;
+        $scope.personalDetail.Gender = $scope.Gender;
+        $scope.personalDetail.DateOfBirth = $scope.DateOfBirth;
+        $scope.personalDetail.IdNumber = $scope.IdNumber;
+        $http({
+                method: "post",
+                url: "http://localhost:18327/Applicants/EditPersonalDetail",
+                datatype: "json",
+                data: JSON.stringify($scope.personalDetail)
+        }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllData();
+                $scope.Title = "";
+                $scope.FirstName = "";
+                $scope.LastName = "";
+                $scope.Gender = "";
+                $scope.DateOfBirth = "";
+                $scope.IdNumber = "";
+                document.getElementById("btnSave").setAttribute("value", "Submit");
+                document.getElementById("btnSave").style.backgroundColor = "cornflowerblue";
+                document.getElementById("spn").innerHTML = "Add New Employee";
+            })  
+    }
+    
+    $scope.GetAllData = function () {
+        $http({
+            method: "get",
+            url: "http://localhost:18327/Applicants/GetApplicant"
+        }).then(function (response) {
+            $scope.personalDetails = response.data;
+        }, function () {
+            alert("Error Occur");
+        })
+    };
+    
+    $scope.UpdateEmp = function (Emp) {
+        document.getElementById("EmpID_").value = Emp.Emp_Id;
+        $scope.EmpName = Emp.Emp_Name;
+        $scope.EmpCity = Emp.Emp_City;
+        $scope.EmpAge = Emp.Emp_Age;
+        document.getElementById("btnSave").setAttribute("value", "Update");
+        document.getElementById("btnSave").style.backgroundColor = "Yellow";
+        document.getElementById("spn").innerHTML = "Update Employee Information";
+    }
+})  
